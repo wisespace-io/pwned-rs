9e2cb72be1bf5fbc2aa14f1d655d5931212f1417
Paul Grandperrin <paul.grandperrin@gmail.com>
2020-11-23 10:32:09 -0300

    update README with new API

--- examples/account.rs
+++ examples/account.rs
@@ -7,7 +7,7 @@ async fn main() {
         .api_key(std::env::var("HIBP_API_KEY").expect("You need to give your HIBP API key as the HIBP_API_KEY environment variable"))
         .build().unwrap();
 
-    match pwned.check_email("flavio@wisespace.io").await {
+    match pwned.check_email("test@wisespace.io").await {
         Ok(answer) => {
             for breach in answer {
                 println!("Service {:?}, breach date {:?} Domain: {:?}", breach.name, breach.breach_date, breach.domain);
