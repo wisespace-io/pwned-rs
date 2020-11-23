5330e69ac070293ddb22a4656936c351a4a6f345
Paul Grandperrin <paul.grandperrin@gmail.com>
2020-11-23 00:14:13 -0300

    upgrade reqwest to 0.10 plus small fixes

--- examples/account.rs
+++ examples/account.rs
@@ -1,13 +1,13 @@
 use pwned::api::*;
-
-fn main() {
+#[tokio::main(basic_scheduler)]
+async fn main() {
     
     let pwned = PwnedBuilder::default()
         .user_agent("my_user_agent")
         .api_key(std::env::var("HIBP_API_KEY").expect("You need to give your HIBP API key as the HIBP_API_KEY environment variable"))
         .build().unwrap();
 
-    match pwned.check_email("flavio@wisespace.io") {
+    match pwned.check_email("flavio@wisespace.io").await {
         Ok(answer) => {
             for breach in answer {
                 println!("Service {:?}, breach date {:?} Domain: {:?}", breach.name, breach.breach_date, breach.domain);
